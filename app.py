706e9f26fe7c0b8be33e4bb644a629b8776b2b67
UNLUCKY000 <62801726+UNLUCKY000@users.noreply.github.com>
2024-01-04 06:46:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 st.title('Speech Generator')
 
 
-embeddings_dataset = st.session_state.cache['EMBED']
-synthesiser = st.session_state.cache['AUDIO_PIPE']
+synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
 speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
 text = st.text_input("Enter text up to 500 words")
